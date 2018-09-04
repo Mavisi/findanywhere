e308aab4a3737025179e7aae88f6ce7b07d60467
Emanuel Cerqueira <emanuelcerqueira07@gmail.com>
2018-09-04 15:49:19 -0300

    Classe GrupoControle (#6)
    * Adicionada classe grupo
    
    * Adicionado metódos addRastreavel e addArea

---
+++ src/model/GrupoControle.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.interfaces.Rastreavel;
+
+public class GrupoControle {
+    
+    private int id;
+    private List<Rastreavel> rastreaveis = new ArrayList<Rastreavel>();
+    private List<Area> areasPermitidas = new ArrayList<Area>();
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public List<Rastreavel> getRastreaveis() {
+        return rastreaveis;
+    }    
+    public void addRastreavel(Rastreavel rastreavel) {
+        this.rastreaveis.add(rastreavel);
+    }
+    public List<Area> getAreasPermitidas() {
+        return areasPermitidas;
+    }
+    public void addArea(Area area) {
+        this.areasPermitidas.add(area);
+    }    
+}
+
